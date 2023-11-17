62cfd4f95d7cb125a489070365f93283d236da8e
Ashok Dhungana <you@example.com>
2023-11-17 17:24:01 +0545

    Java

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args) {
+        int A[]={2,4,6,8,10};
+        for(int i=0;i<A.length;i++){
+            System.out.println(A[i]);
+        }
+        for(int i=A.length-1;i>=0;i--){
+            System.out.println(A[i]);
+        }
+    }
+}
